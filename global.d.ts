64dadc53a61f080696ef880ca74758e872276ad5
Kayla Robertson <kayla.robertson3@gmail.com>
2019-07-31 18:32:07 +0200

    config map custom icon

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,8 @@
-declare module 'console' {
-    export = typeof import("console");
+declare module "console" {
+  export = typeof import("console");
+}
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
 }
--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -4,14 +4,17 @@ import { BREAKPOINT } from "@src/theme";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-
-let DefaultIcon = L.icon({
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-});
+import iggLogo from "@src/icons/igg.svg";
 
 const IGGpos = [52.50108, 13.31798];
-L.Marker.prototype.options.icon = DefaultIcon;
+
+// let DefaultIcon = L.icon({
+//   iconUrl: require("icons/igg.svg"),
+//   // shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+//   iconAnchor: [13, 27]
+// });
+
+// L.Marker.prototype.options.icon = DefaultIcon;
 
 const LeafletWrapper = styled.div`
   margin-bottom: 2rem;
@@ -40,22 +43,40 @@ class MapContainer extends React.Component<
     this.state = {
       lat: 52.50108,
       long: 13.31798,
-      zoom: 13
+      zoom: 16
     };
   }
 
   render() {
     const { cardData } = this.props;
+
+    const myIcon = L.icon({
+      iconUrl: require("icons/igg.svg"),
+      iconSize: [30, 30],
+      iconAnchor: [13, 27],
+      popupAnchor: null,
+      shadowUrl: null,
+      shadowSize: null,
+      shadowAnchor: null
+    });
+
     return (
       <LeafletWrapper>
-        <Map center={IGGpos} zoom={this.state.zoom}>
+        <Map
+          center={IGGpos}
+          zoom={this.state.zoom}
+          scrollWheelZoom={"center"}
+          zoomSnap={0}
+          minZoom={14}
+          maxZoom={17}
+        >
           <TileLayer
             url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
             attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={IGGpos}>
+          <Marker icon={myIcon} position={IGGpos}>
             <Popup>
-              <span>popup</span>
+              <span>IGG Office</span>
             </Popup>
           </Marker>
         </Map>
@@ -65,3 +86,15 @@ class MapContainer extends React.Component<
 }
 
 export default MapContainer;
+
+{
+  /* <TileLayer
+            url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker icon={<img src="icons/igg.svg" />} position={IGGpos}>
+            <Popup>
+              <span>IGG Office</span>
+            </Popup>
+          </Marker> */
+}
