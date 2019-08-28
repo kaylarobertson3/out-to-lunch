acee1aced4f06ee0a8c9efe0a5b2536d16afad9d
Carol Leung <carol.carl.leung@gmail.com>
2019-08-28 16:31:09 +0200

    Revert "refactor: replace class with function component"
    This reverts commit 2b2af4e3c6fe4cd863e209c4d7bd2d7428831f11.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { COLOR, BREAKPOINT } from "@src/theme";
+import { COLOR, BREAKPOINT, FONT } from "@src/theme";
 import starIcon from "@public/assets/icons/star.svg";
 import walkIcon from "@public/assets/icons/walk.svg";
 
@@ -172,52 +172,63 @@ interface CardProps {
   handleCardClick: () => void;
 }
 
-const Card = ({ imgUrl, price, name, rating, distance, tags }: CardProps) => {
-  const priceText = () => {
-    if (price == 1) {
-      return "$";
-    } else if (price == 2) {
-      return "$$";
-    } else if (price == 3) {
-      return "$$$";
-    } else return "?";
-  };
+class Card extends React.Component<CardProps> {
+  render() {
+    const {
+      imgUrl,
+      price,
+      name,
+      rating,
+      distance,
+      tags
+    } = this.props;
 
-  return (
-    <CardLink
-      onClick={e => {
-        this.props.handleCardClick();
-      }}
-    >
-      <CardContainer>
-        <ImgContainer>
-          <Img src={imgUrl} alt={name} />
-          <Details>
-            <Rating>
-              <img src={starIcon} alt="rating" />
-              <h5>{rating}</h5>
-            </Rating>
-            <Distance>
-              <img height="15px" src={walkIcon} alt="walking distance" />
-              <h5>{distance} min.</h5>
-            </Distance>
-          </Details>
-        </ImgContainer>
-        <TextContainer>
-          <NameWrapper>
-            <Name>{name}</Name>
-            <Price>{priceText()}</Price>
-          </NameWrapper>
-          <Row>
-            <Tags>
-              {tags && tags[0] && <Tag>{tags[0]}</Tag>}
-              {tags && tags[1] && <Tag>{tags[1]}</Tag>}
-            </Tags>
-          </Row>
-        </TextContainer>
-      </CardContainer>
-    </CardLink>
-  );
-};
+    const priceText = () => {
+      if (price == 1) {
+        return "$";
+      } else if (price == 2) {
+        return "$$";
+      } else if (price == 3) {
+        return "$$$";
+      } else return "?";
+    };
+
+    return (
+      <CardLink
+        onClick={e => {
+          this.props.handleCardClick();
+        }}
+      >
+        <CardContainer>
+          <ImgContainer>
+            <Img src={imgUrl} alt={name} />
+            <Details>
+              <Rating>
+                <img src={starIcon} alt="rating" />
+                <h5>{rating}</h5>
+              </Rating>
+              <Distance>
+                <img height="15px" src={walkIcon} alt="walking distance" />
+                <h5>{distance} min.</h5>
+              </Distance>
+            </Details>
+          </ImgContainer>
+          <TextContainer>
+            <NameWrapper>
+              <Name>{name}</Name>
+              <Price>{priceText()}</Price>
+            </NameWrapper>
+            <Row>
+              <Tags>
+                {tags && tags[0] && <Tag>{tags[0]}</Tag>}
+                {tags && tags[1] && <Tag>{tags[1]}</Tag>}
+              </Tags>
+            </Row>
+          </TextContainer>
+        </CardContainer>
+      </CardLink>
+    );
+  }
+}
 
 export default Card;
--- src/constants.ts
+++ src/constants.ts
@@ -4,9 +4,5 @@ export const ANIMATION = {
 
 export const IGG_MAP_POSITION = {
   lat: 52.50108,
-  lng: 13.31798,
-  zoom: 14,
-  zoomSnap: 0,
-  minZoom: 14,
-  maxZoom: 17
+  lng: 13.31798
 };
