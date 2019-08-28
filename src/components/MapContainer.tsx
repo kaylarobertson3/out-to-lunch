e51fd94bdd91ab7febd7219140f80fca44dd6d7d
Carol Leung <carol.carl.leung@gmail.com>
2019-08-28 15:58:34 +0200

    refactor: replace class with function component

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -72,92 +72,84 @@ const DirectionsBtn = styled.a`
   /* color: ${COLOR.white}; */
 `;
 
-class MapContainer extends React.Component<
-  {
-    cardData: any;
-    clickedPos: any;
-  },
-  {
-    lat: number;
-    long: number;
-    zoom: number;
-    popupOpen: boolean;
-  }
-> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: 52.50108,
-      long: 13.31798,
-      zoom: 14,
-      popupOpen: false
-    };
-  }
+interface MapContainerProps {
+  cardData: any;
+  clickedPos: string;
+}
 
-  render() {
-    const { cardData, clickedPos } = this.props;
-    return (
-      <LeafletWrapper>
-        <Map
-          center={[IGG_MAP_POSITION.lat, IGG_MAP_POSITION.lng]}
-          zoom={this.state.zoom}
-          scrollWheelZoom={false}
-          touchZoom={false}
-          zoomSnap={0}
-          minZoom={14}
-          maxZoom={17}
+const MapContainer = ({ cardData, clickedPos }: MapContainerProps) => {
+  // lat: number;
+  // long: number;
+  // zoom: number;
+  // popupOpen: boolean;
+
+  // this.state = {
+  //   lat: 52.50108,
+  //   long: 13.31798,
+  //   zoom: 14,
+  //   popupOpen: false
+  // };
+  return (
+    <LeafletWrapper>
+      <Map
+        center={[IGG_MAP_POSITION.lat, IGG_MAP_POSITION.lng]}
+        zoom={this.state.zoom}
+        scrollWheelZoom={false}
+        touchZoom={false}
+        zoomSnap={0}
+        minZoom={14}
+        maxZoom={17}
+      >
+        <TileLayer
+          url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker
+          icon={IggIcon}
+          position={[IGG_MAP_POSITION.lat, IGG_MAP_POSITION.lng]}
         >
-          <TileLayer
-            url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
-            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker
-            icon={IggIcon}
-            position={[IGG_MAP_POSITION.lat, IGG_MAP_POSITION.lng]}
-          >
-            <Popup position={[IGG_MAP_POSITION.lat, IGG_MAP_POSITION.lng]}>
-              <p>Igg Office</p>
-            </Popup>
-          </Marker>
-          {cardData.map((d, i) => {
-            if (d.lat && d.long) {
-              const lat = d.lat;
-              const lng = d.long;
-              const link = `https://www.google.com/maps/search/?api=1&query=${d.lat},${d.long}`;
-              return (
-                <>
-                  <Marker
-                    key={`marker-${d.name}`}
-                    icon={DefaultIcon}
-                    position={[lat, lng]}
-                  >
-                    <Popup position={[lat, lng]}>
-                      <p>{d.name}</p>
-                      {/* <p>{d.rating}</p>
+          <Popup position={[IGG_MAP_POSITION.lat, IGG_MAP_POSITION.lng]}>
+            <p>Igg Office</p>
+          </Popup>
+        </Marker>
+        {cardData.map((d, i) => {
+          if (d.lat && d.long) {
+            const lat = d.lat;
+            const lng = d.long;
+            const link = `https://www.google.com/maps/search/?api=1&query=${d.lat},${d.long}`;
+            return (
+              <>
+                <Marker
+                  key={`marker-${d.name}`}
+                  icon={DefaultIcon}
+                  position={[lat, lng]}
+                >
+                  <Popup position={[lat, lng]}>
+                    <p>{d.name}</p>
+                    {/* <p>{d.rating}</p>
                       <p>{d.price}</p>
                       <p>{d.distance<inutes}</p> */}
-                      <DirectionsBtn href={link}>
-                        <p>Directions -></p>
-                      </DirectionsBtn>
-                    </Popup>
-                  </Marker>
-                  {clickedPos === d.name && (
-                    <Popup position={[lat, lng]}>
-                      <p>{d.name}</p>
-                      {/* <p>{d.rating}</p>
+                    <DirectionsBtn href={link}>
+                      <p>Directions -></p>
+                    </DirectionsBtn>
+                  </Popup>
+                </Marker>
+                {clickedPos === d.name && (
+                  <Popup position={[lat, lng]}>
+                    <p>{d.name}</p>
+                    {/* <p>{d.rating}</p>
                       <p>{d.price}</p>
                       <p>{d.distanceMinutes}</p> */}
-                      <DirectionsBtn href={link}>Directions -></DirectionsBtn>
-                    </Popup>
-                  )}
-                </>
-              );
-            }
-          })}
-        </Map>
-      </LeafletWrapper>
-    );
-  }
-}
+                    <DirectionsBtn href={link}>Directions -></DirectionsBtn>
+                  </Popup>
+                )}
+              </>
+            );
+          }
+        })}
+      </Map>
+    </LeafletWrapper>
+  );
+};
 
 export default MapContainer;
